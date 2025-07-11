f072cc542997d234cf894c538944f07e0e100776
MohamedEmbrator <embrator233@gmail.com>
2025-07-12 02:58:00 +0300

    build project structure

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.static("assets"));
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
-})
+  console.log(`http://localhost:${port}`);
+});
